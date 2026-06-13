9a277b919eaa2c3a1234ae30118305c755935814
gabrielfguimar <ggbrlguimaraes@gmail.com>
2026-06-12 21:40:15 -0300

    Create PortfolioApplicationTests.java

---
+++ src/test/java/com/example/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Teste básico para garantir que o contexto do Spring Boot carrega corretamente
+	}
+}
